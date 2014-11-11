9b725b7988faa6c0998e2948f15fad025f7da517
kunzimariano <kunzi.mariano@gmail.com>
2014-11-11 15:00:44 -0300

    Passing config object to the controllers.

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -1,11 +1,12 @@
 (function(api) {
 
   var githubCommits = require('github-commits'),
-    settingsController = require('./settingsController');
+    settingsController = require('./settingsController'),
+    config = require('../config');
 
   api.init = function(app) {
-    githubCommits.importController.init(app);
-    githubCommits.queryController.init(app);
+    githubCommits.importController.init(app, config);
+    githubCommits.queryController.init(app, config);
     settingsController.init(app);
   };
 })(module.exports);
