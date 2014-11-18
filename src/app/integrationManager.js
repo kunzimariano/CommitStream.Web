0d4782ed9495ab9d7e615b66cbda346e4d59af36
kunzimariano <kunzi.mariano@gmail.com>
2014-11-18 09:40:48 -0300

    integrationManager now handles github urls.

--- src/app/integrationManager.js
+++ src/app/integrationManager.js
@@ -1,11 +1,14 @@
 module.exports.loadIntegrations = function(integrations, cb) {
   integrations.forEach(function(moduleName) {
-    var parts = moduleName.split('/')
+    var tokens = moduleName.split('/')
     // gets the last token from the array which in this case is the module name
     // modulename
     // gitowner/gitrepo
     // ../folder/anotherfolder/modulename
-    var integration = require(parts[parts.length - 1]);
+    var lastToken = tokens[tokens.length - 1];
+    // now for cases like the next git://github.com/user/project.git#branch-or-tag
+    tokens = lastToken.split('.git');
+    var integration = require(tokens[0]);
     cb(integration);
   });
 }
